9fc49c6463fd215c4113c1f9b0d9c642b937840a
gr2nade <gr2nade@gmail.com>
2025-01-06 17:48:56 +0900

    Board Detail

--- src/components/BoardDetail.jsx
+++ src/components/BoardDetail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import apiClient from '../axiosConfig';
-import '../styles/Board.css';
+import './../styles/Board.css';
 
 function BoardDetail() {
     const { id } = useParams();
